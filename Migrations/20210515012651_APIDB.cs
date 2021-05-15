311813ed3d1544090b1ecc85608afc0780243f94
Saurav Saurav <bhandarisaurav940@gmail.com>
2021-05-15 18:30:09 +1200

    Add project files.

---
+++ Migrations/20210515012651_APIDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace Commercial_Adverts_Web_API.Migrations
+{
+    public partial class APIDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommercialDeal",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MainTitle = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Contact = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommercialDeal", x => x.Id);
+                });
+
+            var sqlFile = Path.Combine(".\\DatabaseScripts", @"data.sql");
+
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommercialDeal");
+        }
+    }
+}
